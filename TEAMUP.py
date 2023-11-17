5c1c7dc181ee8a4a67315f2595cc85e93b014a86
janasamra <88239614+janasamra@users.noreply.github.com>
2023-11-17 19:35:36 +0200

    Add files via upload

--- TEAMUP.py
+++ TEAMUP.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1zhVuTtjTjcfk1A__KO-5ZKZOygTyID0t
 """
 
-!pip install streamlit
-
 import streamlit as st
 import pandas as pd
 import pickle
